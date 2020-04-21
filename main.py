36baca5da3618a29093b0c4b765f407a3ed030b6
TheBdouilleur2 <jcodeje@protonmail.com>
2020-04-21 15:30:37 +0200

    Ajout des premières fonctions

--- main.py
+++ main.py
@@ -1,27 +1,73 @@
-import os, sys, subprocess
-
-def createTask(name, options = None):
-    command = "task add '" + name + "' "
-    if not options:
-        pass
-    else:    
-        for option in options.keys():
-            command += option + ":" + options[option]
-
-    os.system(command)
-
-def deleteTask(id):
-    command = "task " + str(id) + " delete"
-    os.system(command)
+import json
 
 def getTasks():
-    with open("tasks.txt", "w") as file:
-        file.write('') #Supression du contenu
+    tasks = []
 
-    command = "task > tasks.txt"
+    with open("tasks.json") as file:
+        fileContent = json.load(file)
 
-    os.system(command)
+    for task in fileContent:
+        tasks.append(task)
 
-    with open('tasks.txt', 'r') as file:
-        tasks = file.read()
-    print(tasks)
+    return tasks
+
+def getTask(ID):
+    tasks = []
+
+    with open("tasks.json") as file:
+        fileContent = json.load(file)
+
+    for task in fileContent:
+        tasks.append(task)
+
+    return tasks[ID]
+
+def writeTasks(tasks):
+    with open("tasks.json", "w") as file:
+        json.dump(tasks, file,  indent=4)
+
+def getLastID():
+    with open("tasks.json") as file:
+        fileContent = json.load(file)
+
+    lastID = fileContent[0]["lastID"]
+    return lastID
+    
+"""
+Ajoute une tache dans le fichier json
+@param params un dictionnaire contenant les parametres à passer.
+"""
+def addTask(name, **params):
+    lastID = getLastID()
+    task = {"ID": lastID + 1, "name":name}
+    if params:
+        for name in params.keys():
+            task[name] = params[name]
+    tasks = getTasks()
+    tasks.append(task)
+    tasks[0]["lastID"] += 1
+    writeTasks(tasks)
+
+def addParams(ID, **params):
+    task = getTask(ID) 
+    if params:
+        for name in params.keys():
+            task[name] = params[name]
+    tasks = getTasks()
+    tasks[ID] = task
+    writeTasks(tasks)
+
+def deleteTask(ID):
+    tasks = getTasks()
+    if ID < len(tasks):
+        del tasks[ID]
+    else:
+        print("La tache n'existe pas!")
+        return False
+    i = 0
+    for task in tasks:
+        task['ID'] = i
+        i += 1
+    tasks[0]["lastID"] -= 1
+    writeTasks(tasks)
+
